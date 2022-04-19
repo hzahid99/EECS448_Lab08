be1c575026ebaa3ba085bf2d7659cf91fb9266a7
Huzaifa Zahid <hzahid99@hotmail.com>
2022-04-18 21:22:41 -0500

    Debugged exercise 04.

--- Exercise04/ex4.js
+++ Exercise04/ex4.js
@@ -1,11 +1,11 @@
-let red1 = document.querySelector('#r1')
-let green1 = document.querySelector('#g1')
-let blue1 = document.querySelector('#b1')
+let red1 = document.querySelector('#red1')
+let green1 = document.querySelector('#green1')
+let blue1 = document.querySelector('#blue1')
 let w = document.querySelector('#w')
 
-let red2 = document.querySelector('#r2')
-let green2 = document.querySelector('#g2')
-let blue2 = document.querySelector('#b2')
+let red2 = document.querySelector('#red2')
+let green2 = document.querySelector('#green2')
+let blue2 = document.querySelector('#blue2')
 
 let change = document.querySelector('#button')
 let paragraph = document.querySelector(".paragraph")
